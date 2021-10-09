dcc860043ea8bfbd2b323cb3771a8c33f447806c
adilku <adhan2a@gmail.com>
2021-10-09 14:06:32 +0300

    final

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dispatcher
-.
+**Пример рассылки**
+
+```
+ //Сборка
+ go mod download -x
+ go build -o dispatcher cmd/dispatcher/main.go
+ 
+ //Запуск
+ //Будем брать данные из data/example.json
+ ./dispatcher data/example.json
+ //данные расслаются в 
+ ```
--- cmd/dispatcher/main.go
+++ cmd/dispatcher/main.go
@@ -1,44 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/slack-go/slack"
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 )
 
 //type messages
 
+type channel struct {
+	Text 	 string `json:"text"`
+	ChanName string `json:"channel"`
+}
+
+type messages struct {
+	BotToken string `json:"bot_token"`
+	Channels  []channel `json:"channels"`
+}
+
+func sendToChan(wg *sync.WaitGroup, mutex *sync.Mutex, chanName string, text string, api *slack.Client) {
+	defer wg.Done()
+	mutex.Lock()
+	log.Println("starting send")
+	_, _, err := api.PostMessage(chanName, slack.MsgOptionText(text, false))
+	if err != nil {
+		log.Println("cant send message", err)
+	}
+	log.Println("send finished")
+	mutex.Unlock()
+}
 
 func main()  {
-
-	file, err := os.Open("../../data/example.json");
+	file, err := os.Open(os.Args[1]);
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	bytes, err := ioutil.ReadAll(file)
-
-
-
-	api := slack.New("")
-
-	attachment := slack.Attachment{
-		Pretext: "some pretext",
-		Text:    "some text",
-	}
-
-	channelID, timestamp, err := api.PostMessage(
-		"C02H5DPC91S",
-		slack.MsgOptionText("Some text", false),
-		slack.MsgOptionAttachments(attachment),
-		slack.MsgOptionAsUser(false), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
-	)
+	mess := messages{}
+	err = json.Unmarshal(bytes, &mess)
 	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
-
+	api := slack.New(mess.BotToken)
+	wg := &sync.WaitGroup{}
+	AllChannels := make(map[string]*sync.Mutex)
+	for _, channel := range mess.Channels {
+		if _, ok := AllChannels[channel.ChanName]; !ok {
+			AllChannels[channel.ChanName] = &sync.Mutex{}
+		}
+		wg.Add(1)
+		go sendToChan(wg, AllChannels[channel.ChanName], channel.ChanName, channel.Text, api)
+	}
+	wg.Wait()
 }
